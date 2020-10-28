78c08f7fb71c773b0fbbdc725818cc4808761461
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2020-10-28 16:30:49 +0100

    added bootstrap, downlaod button almost working it downloading without the file extantion

--- web_app/run.py
+++ web_app/run.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 app = Flask(__name__)
 
-
 @app.route('/')
+@app.route('/qumrax')
 def index():
     return render_template('index.html')
+   
+
+@app.route("/download")
+def download():
+   return send_file("static\\images.zip", add_etags=True, mimetype="application/zip")
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
