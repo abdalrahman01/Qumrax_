4908814e713f5a2196b11a954db069fa3327214a
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2020-11-01 18:52:03 +0100

    cleand som files

--- web_app/run.py
+++ web_app/run.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, send_file
+import socket
+ip = socket.gethostname()
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,8 +11,7 @@ def index():
 
 @app.route("/download")
 def download():
-   return send_file("static\\images.zip", add_etags=True, mimetype="application/zip")
+   return send_file("app-debug.apk", as_attachment=True)
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
- 
+  app.run(host="0.0.0.0", port=80, debug=True)
