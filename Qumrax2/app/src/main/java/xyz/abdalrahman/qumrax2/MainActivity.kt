4de3eca46b18d813845e67a3e036d600dbb90cad
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2020-11-07 15:36:40 +0100

    now it hides the sittings in fullscreen mode

--- Qumrax2/app/src/main/java/xyz/abdalrahman/qumrax2/MainActivity.kt
+++ Qumrax2/app/src/main/java/xyz/abdalrahman/qumrax2/MainActivity.kt
@@ -13,7 +13,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE
+import android.view.View.*
 import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
@@ -25,7 +25,12 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.core.view.marginBottom
+import androidx.core.view.marginStart
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import org.tensorflow.lite.support.common.FileUtil
 import xyz.abdalrahman.qumrax2.ml.SsdMobilenetV11Metadata1
 import java.util.concurrent.ExecutorService
@@ -94,6 +99,11 @@ class MainActivity : AppCompatActivity() {
                 hideSystemUI()
         }
 
+
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
     @SuppressLint("UnsafeExperimentalUsageError")
     private fun openCamera() {
@@ -194,16 +204,6 @@ class MainActivity : AppCompatActivity() {
         )
         startActivity(viewIntent)
     }
-
-
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-
-
-
-    }
-
     private fun hideSystemUI() {
         // Enables regular immersive mode.
         // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
@@ -211,18 +211,22 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = (SYSTEM_UI_FLAG_IMMERSIVE
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+                or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_FULLSCREEN)
+        sittings.visibility = INVISIBLE
     }
     private fun showSystemUI() {
 
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        window.decorView.systemUiVisibility = (SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        if (context.resources.configuration.orientation == 1)
+            sittings.translationY = -100F
+        sittings.visibility = VISIBLE
 
     }
 }
