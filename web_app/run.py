74199ace51b8bbb1598c790d8a07d129ca0024e8
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2020-11-09 19:45:52 +0100

    added app and pics

--- web_app/run.py
+++ web_app/run.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route("/download")
 def download():
-   return send_file("qumrax1-1.apk", as_attachment=True)
+   return send_file("app-release.apk", as_attachment=True)
 
 if __name__ == '__main__':
-  app.run(host="127.0.0.1", port=5000, debug=True)
+  app.run()
