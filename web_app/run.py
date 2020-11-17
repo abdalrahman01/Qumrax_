1b9c17f2857804a87f4b57d36dee9536ba48a419
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2020-11-17 13:05:38 +0100

    added mothed load_labels

--- web_app/run.py
+++ web_app/run.py
@@ -4,15 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   
-  with open("static/label.txt", "r") as labels:
-    
-    return render_template('index.html', labels = labels)
+
+  return render_template('index.html', labels = load_labels())
 
 
 @app.route("/download")
 def download():
    return send_file("app-release.apk", as_attachment=True)
+
+
+def load_labels():
+  with open("static/label.txt", "r") as labels:
+    return labels.readlines()
 
 if __name__ == '__main__':
   app.run(debug=True)
